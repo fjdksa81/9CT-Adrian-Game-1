feb17befd14c0e7def8304c5a93acce5916fa291
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-11-11 12:23:35 +1100

    vibe coding the basics

--- main.py
+++ main.py
@@ -3,6 +3,35 @@
 # Mainloop
 
 while game_over == False
-   while day_count != mission_day_requirement
+   while day_count != mission_day_requirement:
+      print
       day_count = day_count+1
+      days_until_mission = mission_day_requirement-day_count
       
+      if  multi_day_event == False
+          event_check = random_number = random.randint(1, 10)
+          # Long event = 1, only morning = 2/3, only afternoon = 4/5, all day = 6/7, no event = 7-10
+          if event_check == 8 or 9 or 10: 
+             pass
+          elif event_check == 6 or 7:
+             all_day_event()
+             pass
+          elif event_check == 4 or 5:
+             afternoon_event
+             pass
+          elif event_checker == 2 or 3:
+             morning_event
+             pass
+          else:
+             multi_day_event_1
+             pass
+      else:
+         if multi_day_event_count == 1:
+            multi_day_event_2()
+            pass
+         elif multi_day_event_count == 2:
+            multi_day_event_3()
+            pass
+         else: 
+            multi_day_event_4()
+            pass

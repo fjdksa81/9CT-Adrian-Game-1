7d1bf56a46bc1892e8300130da36d230bbee12d9
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-11-25 12:26:38 +1100

    adrian for president 2028

--- events.py
+++ events.py
@@ -3,3 +3,19 @@ import flavortext
 import missions
 import saves
 import stats
+import random
+
+def all_day_event():
+    event_pick =  random.randint(1, 10)
+    if event_pick == 1:
+        print(event_choice_text_1_all_day)
+        event_choice  = input("Insert Your Response: [A,B,C]")
+        if event_choice == "A":
+             
+    elif event_pick == 2:
+
+    elif event_pick == 3:
+
+    elif event_pick == 4:
+
+    else:
